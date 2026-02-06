2975f5d2ed6a3c5be59f9dac3e6b17fd22556017
Jonathan <jonathan@example.com>
2026-02-06 15:47:25 +0000

    Add Colemak Mod-DH window navigation keymaps
    - Map Ctrl+NEIU to window navigation for Glove80 nav layer
    - Aligns with Colemak Mod-DH layout (NEIU = left/down/up/right)
    - Provides consistent navigation between movement and window switching

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -4,3 +4,10 @@
 
 -- vim-tmux-navigator is configured in plugins/tmux-navigator.lua
 -- This provides seamless navigation with Ctrl+hjkl between vim splits and tmux panes
+
+-- Colemak Mod-DH window navigation (NEIU = left/down/up/right on nav layer)
+-- Maps Ctrl+NEIU to window navigation (same as Ctrl+HJKL)
+vim.keymap.set("n", "<C-n>", "<C-h>", { desc = "Go to left window" })
+vim.keymap.set("n", "<C-e>", "<C-j>", { desc = "Go to lower window" })
+vim.keymap.set("n", "<C-i>", "<C-k>", { desc = "Go to upper window" })
+vim.keymap.set("n", "<C-u>", "<C-l>", { desc = "Go to right window" })
