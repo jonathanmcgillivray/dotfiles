58886d8a0d4e190ea9467d547cd7751be5413553
Jonathan <jonathan@example.com>
2026-02-06 16:00:38 +0000

    Fix window navigation keymaps to use Alt instead of Ctrl
    - Change from Ctrl+NEIU to Alt+NEIU to avoid conflicts
    - Ctrl+I conflicts with Tab, Ctrl+E/U are default scroll commands
    - Alt+NEIU are unmapped and safe to use for window navigation

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -6,8 +6,8 @@
 -- This provides seamless navigation with Ctrl+hjkl between vim splits and tmux panes
 
 -- Colemak Mod-DH window navigation (NEIU = left/down/up/right on nav layer)
--- Maps Ctrl+NEIU to window navigation (same as Ctrl+HJKL)
-vim.keymap.set("n", "<C-n>", "<C-h>", { desc = "Go to left window" })
-vim.keymap.set("n", "<C-e>", "<C-j>", { desc = "Go to lower window" })
-vim.keymap.set("n", "<C-i>", "<C-k>", { desc = "Go to upper window" })
-vim.keymap.set("n", "<C-u>", "<C-l>", { desc = "Go to right window" })
+-- Maps Alt+NEIU to window navigation (avoids Ctrl+I/E/U conflicts)
+vim.keymap.set("n", "<M-n>", "<C-h>", { desc = "Go to left window" })
+vim.keymap.set("n", "<M-e>", "<C-j>", { desc = "Go to lower window" })
+vim.keymap.set("n", "<M-i>", "<C-k>", { desc = "Go to upper window" })
+vim.keymap.set("n", "<M-u>", "<C-l>", { desc = "Go to right window" })
